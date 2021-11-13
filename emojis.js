529f7fd27e060e085f60afbc1e58cb2054573fd4
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2021-11-13 21:31:00 +0100

    Update and rename test to emojis.js

---
+++ emojis.js
@@ -0,0 +1,13 @@
+client.on("message", message => {
+  if (!message.guild) return
+  let args = message.content.trim().split(/ +/g)
+
+  if (args[0].toLowerCase() === prefix + "clear") {
+      if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Vous n'avez pas la permission d'effectuer cette commande")
+      let count = args[1]
+      if (!count) return message.channel.send("Veuillez indiquer un nombre de message à supprimer")
+      if (isNaN(count)) return message.channel.send("Veuillez indiquer un nombre valide")
+      if (count < 1 || count > 100) return message.channel.send("Veuillez indiquer un nombre entre 1 et 100")
+      message.channel.bulkDelete(parseInt(count) + 1)
+  }
+})
